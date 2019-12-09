a4cd6216c69d9e72e1262905a37c57b82372f911
Monika <mswidzinska87@gmail.com>
2019-12-09 15:01:33 +0100

    done

---
+++ starter-code/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/public/about.html");
+})
+app.get("/gallery", (req, res) => {
+    res.sendFile(__dirname + "/public/photo-gallery.html");
+})
+
+app.get("/test", (req, res, next) => {
+    res.send("it works")
+    console.log('true')
+})
+app.get("*", (req, res, next) => {
+    res.status(404)
+    console.log('true')
+})
+
+
+app.listen(3003);
+// app.use(express.static("public"));
+
+
+//nodemon app2.js
